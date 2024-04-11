ea33f25eb55097d46a3dece4030509ecb63258c3
anita.schiller <anita.schiller@gutefrage.net>
2024-04-11 15:04:06 +0200

    Update card margin

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({backgroundColor})=> (
-    <section style={{background: backgroundColor, height: '50px'}}>
+    <section style={{background: backgroundColor, height: '50px', margin: '10px'}}>
         <h2>How to do Visual Testing with Storybook</h2>
     </section>
 );
