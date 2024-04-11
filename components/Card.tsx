c60a7d7df8e43f3f396e6ac522f3ec64f794ad6c
anita.schiller <anita.schiller@gutefrage.net>
2024-04-11 13:50:17 +0200

    Setup Chromatic

---
+++ components/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface CardProps {
+    backgroundColor: string;
+}
+
+const Card: React.FC<CardProps> = ({backgroundColor})=> (
+    <section style={{background: backgroundColor, height: '200px'}}>
+        <h2>How to do Visual Testing with Storybook</h2>
+    </section>
+);
+
+export default Card;
