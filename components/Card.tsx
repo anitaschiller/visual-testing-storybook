bb4435e6b1f45815d3b913d3637e03fa500b82a6
anita.schiller <anita.schiller@gutefrage.net>
2024-04-11 14:41:32 +0200

    Update card height again

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,7 +5,7 @@ interface CardProps {
 }
 
 const Card: React.FC<CardProps> = ({backgroundColor})=> (
-    <section style={{background: backgroundColor, height: '100px'}}>
+    <section style={{background: backgroundColor, height: '50px'}}>
         <h2>How to do Visual Testing with Storybook</h2>
     </section>
 );
