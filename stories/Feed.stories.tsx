12e31d4c21a45dc6d5d073ed7e0e1d9b73781f91
anita.schiller <anita.schiller@gutefrage.net>
2024-04-29 13:04:10 +0200

    Update stories

--- stories/Feed.stories.tsx
+++ stories/Feed.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Feed from "@/app/feed";
 
 const meta = {
-    title: 'Feed',
+    title: 'Pages/Feed',
     component: Feed,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
--- stories/Home.stories.tsx
+++ stories/Home.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Home from "@/app/page";
 
 const meta = {
-    title: 'Home',
+    title: 'Pages/Home',
     component: Home,
     parameters: {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
